f29af38b9293536e45bb9fcdcd687cf9c6d455f3
vlad-krivko <154326692+vlad-krivko@users.noreply.github.com>
2023-12-19 19:53:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Кривко В. А.";
 
-  console.log('This is App ');
+
   return (
     <div className="App">
      <header className="App-header">
